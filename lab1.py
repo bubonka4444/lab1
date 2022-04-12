4b1ef70a5f8e0966818d459d59e99a4e9542c768
bubonka4444 <95764454+bubonka4444@users.noreply.github.com>
2022-04-12 21:26:34 +0400

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -5,29 +5,32 @@
 
 import time
 
-start = time.time()                                                 # запуск таймера
+start = time.time()                                                 # начинаем отсчет
 
 buffer_len = 1                                                      # размер буфера чтения
 position = 0                                                        # номер позиции цифры
+f = []                                                              # список для вывода
 
 try:
     with open("prikol.txt", "r") as file:                           # открываем файл
         buffer = file.read(buffer_len)
 
         while buffer:                                               # пока в файле что-то есть
-            while (buffer < '0' or buffer > '9') and buffer:        # поиск чисел
+            while (buffer < '0' or buffer > '9') and buffer:        # пока это цифра
                 buffer = file.read(buffer_len)
 
             while (buffer >= '0' and buffer <= '9') and buffer:     # обработка чисел
                 position += 1
                 if int(buffer) % 2 == 0:
                     for i in range(position):
-                        print(int(buffer))
+                        f.append(int(buffer))
+                    print(f)
+                    f.clear()
                 buffer = file.read(buffer_len)
                 break
 
 except FileNotFoundError:
     print("\nФайл не обнаружен.\nДобавьте файл в директорию или переименуйте существующий файл.")
 
-result = time.time() - start                                        
+result = time.time() - start                                        # заканчиваем отсчет
 print(" Время работы программы: {:>.10f}".format(result))
